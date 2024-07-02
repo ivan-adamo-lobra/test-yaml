99e5dc076ae041675611bd08946a6767b0e68e2b
ivan-adamo-lobra <ivan.adamo@lobra.com>
2024-07-02 11:23:17 +0200

    prova2

--- yaml_separator.py
+++ yaml_separator.py
@@ -6,14 +6,16 @@ def load_yaml_file(filepath):
         yaml_content = yaml.safe_load(file)
     return yaml_content
 
-def filter_get_methods(yaml_content):
-    """ Filtra il contenuto YAML per mantenere solo i metodi 'get'. """
+def filter_get_methods_and_remove_responses(yaml_content):
+    """ Filtra il contenuto YAML per mantenere solo i metodi 'get' e rimuovere le risposte. """
     filtered_content = yaml_content.copy()  # Copia il contenuto originale per preservarlo
     if 'paths' in filtered_content:
         for path in list(filtered_content['paths'].keys()):  # Utilizza list() per evitare modifiche durante l'iterazione
             if 'get' in filtered_content['paths'][path]:
                 # Mantieni solo il metodo 'get' per ogni path
                 get_details = filtered_content['paths'][path]['get']
+                # Rimuovi la sezione 'responses' se presente
+                get_details.pop('responses', None)
                 filtered_content['paths'][path] = {'get': get_details}
             else:
                 # Rimuovi i path che non hanno un metodo 'get'
@@ -27,7 +29,7 @@ def write_yaml_file(data, filename):
 
 def main():
     yaml_content = load_yaml_file("API_BUSINESS_PARTNER.yaml")
-    filtered_content = filter_get_methods(yaml_content)
+    filtered_content = filter_get_methods_and_remove_responses(yaml_content)
     write_yaml_file(filtered_content, "filtered_output.yaml")
 
 if __name__ == "__main__":
